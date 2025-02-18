956e9e5e85934889b6874a0ff5b007a8e49bc149
risabkshetri <risawgc@gmail.com>
2025-02-18 19:35:34 +0530

    small content error fixed

--- src/components/canvas/index.js
+++ src/components/canvas/index.js
@@ -1,9 +1,8 @@
 import EarthCanvas from "./Earth";
 import BallCanvas from "./Ball";
-import ComputersCanvas from "./Computers";
 import StarsCanvas from "./Stars";
 
 import SphereCanvas from "./Sphere";
 
 
-export { EarthCanvas, BallCanvas, ComputersCanvas, StarsCanvas, SphereCanvas };
+export { EarthCanvas, BallCanvas, StarsCanvas, SphereCanvas };
