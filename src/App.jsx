3a90e26cc914d31b02a36d9d787bf6ee38a9c433
risabkshetri <risawgc@gmail.com>
2025-02-18 19:00:22 +0530

    email service added

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,13 @@
 import { BrowserRouter } from "react-router-dom";
+import { useEffect } from 'react';
+import emailjs from '@emailjs/browser';
 
 import { About, Contact, Experience, Hero, Navbar, Tech, Works, StarsCanvas } from "./components";
 
 const App = () => {
+  useEffect(() => {
+    emailjs.init(import.meta.env.VITE_APP_EMAILJS_PUBLIC_KEY);
+  }, []);
   return (
     <BrowserRouter>
       <div className='relative z-0 bg-primary'>
--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -36,9 +36,9 @@ const Contact = () => {
         import.meta.env.VITE_APP_EMAILJS_TEMPLATE_ID,
         {
           from_name: form.name,
-          to_name: "JavaScript Mastery",
+          to_name: "Rishab Chheetri",
           from_email: form.email,
-          to_email: "rishab@kshetriai.com",
+          to_email: "risawgc@gmail.com",
           message: form.message,
         },
         import.meta.env.VITE_APP_EMAILJS_PUBLIC_KEY
